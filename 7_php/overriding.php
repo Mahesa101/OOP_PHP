2c8b2abc1817563f5ccf9af6e5ae67a943246761
mahesa <mahesaagsa101@gmail.com>
2026-04-22 20:04:32 +0700

    overriding

--- 7_php/overriding.php
+++ 7_php/overriding.php
@@ -40,19 +40,28 @@ class Jasa
     }
 }
 
-//cara membuat sebuah hirarki dengan tujuan agar mempermudah membuat nilai spesial setiap object
+// inthitance adalah cara membuat sebuah hirarki dengan tujuan agar mempermudah membuat nilai spesial setiap object
 class A extends Jasa
 {
+public $gramKertas;
 
+public function __construct($nama = "Customer", $jenis = "Jenis", $jumlah = "Jumlah", $harga = "Harga", $tipe = "ukuran",$gram ="Grama kertas")
+{
+    parent::__construct($nama, $jenis, $jumlah, $harga, $tipe);
+    $this->gramKertas =$gram;
+}
+
+        //overriding adalah cara untuk "mengambil alih" parent nya dimana dia dapat menimpa sebuah method dengan kondisi yang di sesuaikan
     public function data()
     {
-        $hasil = "Ukuran {$this->Ukuran} : {$this->namaPelanggan} | {$this->jenisDokumen} | {$this->jumlahCetak} | {$this->hargaPerLembar}/perlembar ";
+        //dimana parent disini mengambil method dari parent nya
+        $hasil = parent:: data();
         if ($this->Ukuran == "A4") {
-            $hasil .= "| Detail : 21,0 x 29,7 cm";
+            $hasil .= "| Detail : 21,0 x 29,7 cm | {$this->gramKertas} gsm";
         } elseif ($this->Ukuran == "A3") {
-            $hasil .= "| Detail : 29,7 x 42,0 cm";
+            $hasil .= "| Detail : 29,7 x 42,0 cm | {$this->gramKertas} gsm";
         } elseif ($this->Ukuran == "A5") {
-            $hasil .= "| Detail : 14,8 x 21,0 cm";
+            $hasil .= "| Detail : 14,8 x 21,0 cm | {$this->gramKertas} gsm";
         } else {
             echo "<script> alert('ukuran tidak tersedia')</script>";
         }
@@ -62,14 +71,20 @@ class A extends Jasa
 
 class B extends Jasa
 {
+public $jenisJilid;
+public function __construct($nama = "Customer", $jenis = "Jenis", $jumlah = "Jumlah", $harga = "Harga", $tipe = "ukuran",$jilid ="jenis Jilid")
+{
+    parent::__construct($nama, $jenis, $jumlah, $harga, $tipe);
+    $this->jenisJilid =$jilid;
+}
 
     public function data()
     {
-        $hasil = "Ukuran {$this->Ukuran} : {$this->namaPelanggan} | {$this->jenisDokumen} | {$this->jumlahCetak} | {$this->hargaPerLembar}/perlembar ";
+        $hasil = parent::data();
         if ($this->Ukuran == "B5") {
-            $hasil .= "| Detail : 17,6 x 25,0 cm";
+            $hasil .= "| Detail : 17,6 x 25,0 cm | {$this->jenisJilid}";
         } elseif ($this->Ukuran == "B4") {
-            $hasil .= "| Detail : 25,0 x 35,3 cm";
+            $hasil .= "| Detail : 25,0 x 35,3 cm | {$this->jenisJilid}";
         } else {
             echo "<script> alert('ukuran tidak tersedia')</script>";
         }
@@ -81,15 +96,22 @@ class B extends Jasa
 class R extends Jasa
 {
 
+public $kualitasFoto;
+public function __construct($nama = "Customer", $jenis = "Jenis", $jumlah = "Jumlah", $harga = "Harga", $tipe = "ukuran",$foto ="kualitas Foto")
+{
+    parent::__construct($nama, $jenis, $jumlah, $harga, $tipe);
+    $this->kualitasFoto =$foto;
+}
+
     public function data()
     {
         $hasil = "Ukuran {$this->Ukuran} : {$this->namaPelanggan} | {$this->jenisDokumen} | {$this->jumlahCetak} | {$this->hargaPerLembar}/perlembar ";
         if ($this->Ukuran == "2R") {
-            $hasil .= "| Detail : 6,0 x 9,0 cm";
+            $hasil .= "| Detail : 6,0 x 9,0 cm | {$this->kualitasFoto}HD ";
         } elseif ($this->Ukuran == "4R") {
-            $hasil .= "| Detail : 10,2 x 15,2 cm";
+            $hasil .= "| Detail : 10,2 x 15,2 cm | {$this->kualitasFoto}4K";
         } elseif ($this->Ukuran == "10R") {
-            $hasil .= "| Detail : 125,4 x 30,5 cm";
+            $hasil .= "| Detail : 125,4 x 30,5 cm | {$this->kualitasFoto}4K";
         } else {
             echo "<script> alert('ukuran tidak tersedia')</script>";
         }
@@ -116,7 +138,7 @@ class info
 }
 
 
-$cetakA = new A("Ujang", "JPG", 10, 1000, "A4");
+$cetakA = new A("Ujang", "JPG", 10, 1000, "A4",80);
 $cetakB = new B("Ani", "PNG", 20, 1500, "B5");
 
 
